36215da8e019bd066b17e9e437b853596094f687
Halle Helfrich <hallehelfrich@Halles-MacBook-Air.local>
2021-01-29 19:23:09 -0600

    added person and database file

---
+++ employeeDatabase.py
@@ -0,0 +1,14 @@
+import personEmploy
+
+running = True
+employees = []
+while (running):
+    Print("Options: \n AddNewEmployee \n DeleteEmployee \n SeeList \n UploadList \n Exit")
+    if (input() == "Exit"):
+        running = False
+    if (input() == "AddNewEmployee"):
+        getPerson(self)
+    if (input() == "DeleteEmployee"):
+
+    if (input() == "SeeList"):
+        Print()
---
+++ personEmploy.py
@@ -0,0 +1,46 @@
+class Person:
+    def __init__(self, firstname = "J.", lastname = "Doe", number = "1111111111", email = "jdoe@company", dateemployed = "01/01/2000", department = "IT", jobtitle = "intern"):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.number = number
+        self.email = email
+        self.dateemployed = deteemployed
+        self.department = department
+        self.jobtitle = jobtitle
+
+    def getPerson(self):
+        firstname, lastname = input("Hello! What is your full name?: ").split(" ")
+        self.firstname = firstname
+        self.lastname = lastname
+        number = input("What is your phone number?: ")
+        self.number = number
+        email = input("What is your email address?: ")
+        self.email = email
+        dateemployed = input("What date were you employed? (mm/dd/yyyy): ")
+        self.dateemployed = dateemployed
+        department = input("What department are you in?: ")
+        self.department = department
+        jobtitle = input("What is your job title?: ")
+        self.jobtitle = jobtitle
+    
+    def setFirstname(self, firstname):
+        self.firstname = firstname
+    
+    def setLastname(self, lastname):
+        self.lastname = lastname
+
+    def setNumber(self, number):
+        self.number = number
+    
+    def setEmail(self, email):
+        self.email = email
+
+    def setDateemployed(self, dateemployed):
+        self.dateemployed = dateemployed
+    
+    def setDepartment(self, department):
+        self.department = department
+
+    def setJobtitle(self, jobtitle):
+        self.jobtitle = jobtitle
+        
